09c06247b023d688c29cf08b3790a0a8f64d9501
shafakyildiz <shafakyildiz@gmail.com>
2024-02-11 13:40:38 +0300

    adding service and model for books

--- SwaggerProject/Program.cs
+++ SwaggerProject/Program.cs
@@ -1,9 +1,18 @@
+using BookStoreApi.Services;
+using SwaggerProject.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
 
 var app = builder.Build();
 
